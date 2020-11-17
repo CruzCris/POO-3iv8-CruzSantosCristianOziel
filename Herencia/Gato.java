bd8274fd418c612382790f537c22e1fa168a6e46
Cristian <cruz.santos.cristianoziel@gmail.com>
2020-11-17 17:58:12 -0600

    Clase hija gato

---
+++ Herencia/Gato.java
@@ -0,0 +1,25 @@
+public class Gato extends Animal{
+    private int num_vidas;
+    public Gato(){
+
+    }
+    public Gato(String nombre, String raza String tipo_alimento, int edad, int num_vidas){
+        super(nombre, raza, tipo_salimento, edad);
+        //acceso a la variable propia de la clase
+        this.num_vidas = num_vidas;
+    }
+    public int getNumVidas(){
+        return num_vidas;
+    }
+    //establecer
+    public void setNumVidas(int num_vidas){
+        this.num_vidas = num_vidas;
+    }
+    public void MostrarGato(){
+        System.out.println("El nombre del perro es: "+ getNombre()
+                                + " su raza es: "+ getRaza()
+                                + " come: "+ getTipoAlimento()
+                                + " su edad es de: "+ getEdad()
+                                + " su ladrido es: "+ getNumVidas());
+    }
+}
