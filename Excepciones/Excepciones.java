e347e68cd24acb6fe826bd967d8e7f4eefbfec26
Cristian <cruz.santos.cristianoziel@gmail.com>
2020-11-17 17:21:02 -0600

    Ejemplo Excepciones

---
+++ Excepciones/Excepciones.java
@@ -0,0 +1,10 @@
+//ejmplo del manejo de excepciones para un programa y asi entender 
+//los tipos de error , aplicaremos elementos de separacion de clases
+public class Excepciones{
+    public static void main(String[] args){
+        while(true){
+            OP obj = new OP();
+            obj.Ejemplo();
+        }
+    }
+}
---
+++ Excepciones/OP.java
@@ -0,0 +1,20 @@
+//que pasa si ponen letras en vez de numeros
+import java.util.*;
+public class OP{
+    int y=0;
+    Scanner entrada = new Scanner(System.in);
+    public void Ejemplo(){
+        try{
+            System.out.println("Ingresa un valor numerico: ");
+            y=entrada.nextInt();
+            System.out.println("El numero es: "+y);
+            System.exit(0);
+            //cacha el error mediante el objeto e
+        }catch(Exception e){
+            //mensaje de error
+            System.out.println("Ingrese solo valores enteros");
+            //saber el tipo de error
+            System.out.println("Error: "+ e.getMessage());
+        }
+    }
+}
